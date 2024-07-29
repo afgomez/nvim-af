acc5e9260ab4a13301ce2c356a87741c8084ee3c
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2024-07-29 19:36:29 +0200

    Add keymaps

---
+++ config/keymaps.nix
@@ -0,0 +1,31 @@
+let
+  setKeymap = mode: key: action: options: {
+    mode = mode;
+    key = key;
+    action = action;
+    options = options;
+  };
+in {
+  keymaps = [
+    # Remap `j` and `k` to work with wrapped lines
+    (setKeymap "n" "k" "v:count == 0 ? 'gk' : 'k'" {
+      silent = true;
+      expr = true;
+    })
+
+    (setKeymap "n" "j" "v:count == 0 ? 'gj' : 'j'" {
+      silent = true;
+      expr = true;
+    })
+
+    # Quickly switch between buffers
+    (setKeymap "n" "<leader><leader>" "<C-^>" {
+      noremap = true;
+      silent = true;
+      desc = "Switch to previous buffer";
+    })
+
+    # Diagnostics
+    (setKeymap "n" "<leader>dl" "vim.diagnostic.setloclist" {desc = "Open diagnostics list";})
+  ];
+}
