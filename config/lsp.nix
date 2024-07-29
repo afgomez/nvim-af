1aa862ab123cedfa31025645929cdf74b5e93100
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2024-07-29 13:55:14 +0200

    Setup front-end LSPs

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,4 +1,8 @@
-{
+{pkgs, ...}: let
+  # This is the package that `nixvim` uses for vue-language-server
+  # Change if needed
+  volarOutPath = pkgs.nodePackages."@volar/vue-language-server".outPath;
+in {
   plugins = {
     fidget.enable = true;
 
@@ -6,6 +10,29 @@
       enable = true;
       servers = {
         nixd.enable = true;
+
+        # Front-end development
+        cssls.enable = true;
+        eslint.enable = true;
+        html.enable = true;
+        jsonls.enable = true;
+        tsserver = {
+          enable = true;
+          # Use vue-language-server hybrid mode
+          filetypes = ["typescript" "javascript" "typescriptreact" "javascriptreact" "vue"];
+          extraOptions = {
+            init_options = {
+              plugins = [
+                {
+                  name = "@vue/typescript-plugin";
+                  location = "${volarOutPath}/lib/node_modules/@vue/language-server";
+                  languages = ["vue"];
+                }
+              ];
+            };
+          };
+        };
+        volar.enable = true;
       };
     };
   };
