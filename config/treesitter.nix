64c21ee406e6cbc5f5c1500051a2c7bd8c047e8d
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2024-07-29 11:47:09 +0200

    Correctly comment in embedded languages

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -15,5 +15,21 @@
 
     # Auto-close HTML tags
     ts-autotag.enable = true;
+
+    # Set commentstring on embedded languages (i.e. Vue, Markdown blocks, etc).
+    ts-context-commentstring = {
+      enable = true;
+      disableAutoInitialization = true;
+    };
   };
+
+  # Calcuulates `commentstring` only when needed
+  extraConfigLua = ''
+    local get_option = vim.filetype.get_option
+    vim.filetype.get_option = function(filetype, option)
+      return option == "commentstring"
+        and require("ts_context_commentstring.internal").calculate_commentstring()
+        or get_option(filetype, option)
+    end
+  '';
 }
