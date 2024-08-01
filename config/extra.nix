dccc47bef2929c421a39147f660046fe605fcc9f
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2024-08-01 22:02:38 +0200

    Use `nvim-surround` instead of the vimscript version

--- config/extra.nix
+++ config/extra.nix
@@ -8,16 +8,17 @@
 
     # Insert closing character
     nvim-autopairs.enable = true;
-
-    # Surround
-    surround.enable = true;
   };
 
   extraPlugins = with pkgs.vimPlugins; [
     # `[` `]` mappings
     vim-unimpaired
+
     # emacs-like movements in insert and command mode
     vim-rsi
+
+    # Surround
+    nvim-surround
 
     # Icons everywhere
     nvim-web-devicons
@@ -25,5 +26,8 @@
 
   extraConfigLua = ''
     require('nvim-web-devicons').setup()
+    require('nvim-surround').setup({
+      move_cursor = "sticky",
+    })
   '';
 }
