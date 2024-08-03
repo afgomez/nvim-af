8d54bd79c05a19bd6b8e56b27bebedd166bb8ac7
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2024-08-03 20:17:41 +0200

    Don't close the quickfix window after selecting an element

--- config/after.nix
+++ config/after.nix
@@ -14,7 +14,6 @@
       vim.opt_local.textwidth = 0
 
       vim.keymap.set('n', '<Esc>', ':cclose<bar>lclose<CR>', { buffer = true, silent = true })
-      vim.keymap.set('n', '<CR>', '<CR> :cclose<bar>lclose<CR>', { buffer = true, silent = true, noremap = true })
     '';
   };
 }
