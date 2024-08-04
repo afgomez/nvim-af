c9cd1e342b9438d3db304207895150e9c54265a8
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2024-08-04 19:17:55 +0200

    Use nvim-cmp also in Command/Ex mode

--- config/completion.nix
+++ config/completion.nix
@@ -1,39 +1,57 @@
 {
-  plugins = {
-    cmp = {
-      enable = true;
-      autoEnableSources = true;
-      settings = {
+  plugins.cmp = {
+    enable = true;
+    autoEnableSources = true;
+    settings = {
+      sources = [
+        {name = "nvim_lsp";}
+        {name = "path";}
+        {name = "buffer";}
+      ];
+      mapping = {
+        __raw = ''
+          cmp.mapping.preset.insert({
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+          })
+        '';
+      };
+      # window = {
+      #   completion.border = "rounded";
+      #   documentation.border = "rounded";
+      # };
+    };
+
+    filetype = {
+      gitcommit.sources = [{name = "git";} {name = "path";}];
+      gitrebase.sources = [{name = "git";} {name = "path";}];
+      jj.sources = [{name = "path";}];
+    };
+
+    # Use nvim-cmp also in Command/Ex mode
+    cmdline = {
+      "/" = {
+        mapping = {__raw = "cmp.mapping.preset.cmdline()";};
+        sources = [{name = "buffer";}];
+      };
+      ":" = {
+        mapping = {__raw = "cmp.mapping.preset.cmdline()";};
         sources = [
-          {name = "nvim_lsp";}
           {name = "path";}
-          {name = "buffer";}
+          {
+            name = "cmdline";
+            option = {
+              ignore_cmds = ["Man" "!"];
+            };
+          }
         ];
-        mapping = {
-          __raw = ''
-            cmp.mapping.preset.insert({
-              ['<C-Space>'] = cmp.mapping.complete(),
-              ['<CR>'] = cmp.mapping.confirm({ select = true }),
-              ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-            })
-          '';
-        };
-        # window = {
-        #   completion.border = "rounded";
-        #   documentation.border = "rounded";
-        # };
-      };
-
-      filetype = {
-        gitcommit.sources = [{name = "git";} {name = "path";}];
-        gitrebase.sources = [{name = "git";} {name = "path";}];
-        jj.sources = [{name = "path";}];
       };
     };
+  };
 
-    lspkind = {
-      enable = true;
-      cmp.enable = true;
-    };
+  plugins.lspkind = {
+    enable = true;
+    cmp.enable = true;
   };
 }
