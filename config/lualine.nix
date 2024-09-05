74ac1ab0bfecfc56a08ffce6cef4d03a634b75a1
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2024-09-05 10:20:16 +0200

    Update deps

--- config/lualine.nix
+++ config/lualine.nix
@@ -1,26 +1,21 @@
 {
   plugins.lualine = {
     enable = true;
+    settings = {
+      sections = {
+        lualine_b = ["branch"];
+        lualine_c = [
+          "diagnostics"
+          {__raw = ''{ "filename", path = 1 }'';}
+        ];
+        lualine_x = ["filetype"];
+      };
 
-    sections = {
-      lualine_b = ["branch"];
-      lualine_c = [
-        "diagnostics"
-        {
-          name = "filename";
-          extraConfig = {path = 1;};
-        }
-      ];
-      lualine_x = ["filetype"];
-    };
-
-    inactiveSections = {
-      lualine_c = [
-        {
-          name = "filename";
-          extraConfig = {path = 1;};
-        }
-      ];
+      inactiveSections = {
+        lualine_c = [
+          {__raw = ''{ "filename", path = 1 }'';}
+        ];
+      };
     };
   };
 }
