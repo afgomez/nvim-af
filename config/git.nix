365580f06ac93ed29a4596fc133665ebc991094e
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2024-09-12 14:41:29 +0200

    Tweak the blame line

--- config/git.nix
+++ config/git.nix
@@ -6,7 +6,6 @@
       settings = {
         current_line_blame = true;
         current_line_blame_opts.delay = 500;
-        current_line_blame_formatter = "       <author>, <author_time:%R> • <summary>";
       };
     };
   };
